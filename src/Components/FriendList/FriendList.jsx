8ed620e95cbb05dae53167853b7e4444c86d2c9f
Artem <sava1399ii@gmail.com>
2025-01-15 11:59:29 +0200

    done dz

---
+++ src/Components/FriendList/FriendList.jsx
@@ -0,0 +1,23 @@
+import s from "./FriendList.module.css";
+import FriendListItem from "../FriendListItem/FriendListItem";
+
+const FriendList = ({ friends }) => {
+  return (
+    <div className="container">
+      <h2 className="title">Friends List</h2>
+      <ul className={s.list}>
+        {friends.map((friend) => (
+          <li key={friend.id} className={s.item}>
+            <FriendListItem
+              avatar={friend.avatar}
+              name={friend.name}
+              isOnline={friend.isOnline}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FriendList;
---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,40 @@
+import s from "./Profile.module.css";
+
+const Profile = ({
+  name,
+  tag,
+  location,
+  image = "https://cdn-icons-png.flaticon.com/512/1077/1077012.png",
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <div className="container">
+      <h2 className="title">Profile</h2>
+      <div className={s.wrapper}>
+        <div className={s.user}>
+          <img className={s.img} src={image} alt="User avatar" />
+          <p className={s.name}>{name}</p>
+          <p className={s.tag}>@{tag}</p>
+          <p className={s.location}>{location}</p>
+        </div>
+
+        <ul className={s.userList}>
+          <li className={s.listItem}>
+            <span>Followers</span>
+            <span className={s.bold}>{followers}</span>
+          </li>
+          <li className={s.listItem}>
+            <span>Views</span>
+            <span className={s.bold}>{views}</span>
+          </li>
+          <li className={s.listItem}>
+            <span>Likes</span>
+            <span className={s.bold}>{likes}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
